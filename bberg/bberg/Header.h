52a53976d1fb0e47106ac75fe4b3ed6e66074489
Art Noble <arnoble@btinternet.com>
2014-02-06 17:17:56 +0000

    Bberg - start to get BID and ASK product prices

---
+++ bberg/bberg/Header.h
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <numeric>
+#include <windows.h>
+#include <sqlext.h>
+#include <stdio.h>
+#include <vector>
+#include <regex>
+#include <blpapi_session.h> 
+#include <blpapi_event.h> 
+#include <blpapi_message.h> 
+#include <blpapi_request.h> 
+#include <iostream> 
+using namespace BloombergLP;
+using namespace blpapi;
+
+// *************** STRUCTS
+typedef struct bbergData { double bid, ask; } BbergData;
+
+
+
+// Bberg send request to service
+double getBbergPrice(char *ticker, char *field, blpapi::Service &ref, blpapi::Session &session){
+	double thePrice;
+	
+	blpapi::Request request = ref.createRequest("HistoricalDataRequest");
+	request.getElement("securities").appendValue(ticker);
+	request.getElement("fields").appendValue(field);
+	//request.set("periodicityAdjustment", "ACTUAL");
+	request.set("periodicitySelection", "DAILY");
+	request.set("startDate", "20140205");
+	request.set("endDate", "20140205");
+	//request.set("maxDataPoints", 1);
+	session.sendRequest(request);
+	request.print(std::cout);
+
+	// process response
+	bool done(false);
+	while (!done) {
+		Event event = session.nextEvent();
+		MessageIterator msgIter(event);
+		while (msgIter.next()) {
+			Message msg = msgIter.message();
+			if (Event::RESPONSE == event.eventType() || Event::PARTIAL_RESPONSE == event.eventType()) {
+				if (msg.hasElement("securityData")) {
+					done = true;
+					Element secs = msg.getElement("securityData");
+					std::cout << "\n" << secs.getElementAsString("security") << std::endl;
+					Element flds = secs.getElement("fieldData");
+					// We get an array of value for the historical request.
+					std::cout << "Date\t\tPX_LAST" << std::endl;
+					for (int i = 0; i < flds.numValues(); ++i) {
+						Element f = flds.getValueAsElement(i);
+						thePrice = f.getElementAsFloat64(field);
+						std::cout << f.getElementAsString("date") << "\t" << thePrice << std::endl;
+					}
+				}
+			}
+			else {
+				std::cout << msg << std::endl;
+			}
+		}
+	}
+	return(thePrice);
+}
+
+
+
+class MyDB {
+private:
+	SQLHENV   hEnv;
+	SQLHDBC   hDBC;
+	SQLRETURN fsts;
+	const int bufSize=256;
+	SQLLEN    cbModel;		               // Model buffer bytes recieved
+	HSTMT     hStmt;
+	char     **bindBuffer;
+
+public:
+	// get info on SQL error
+	void extract_error(
+		char *fn,
+		SQLHANDLE handle,
+		SQLSMALLINT type)
+	{
+		SQLINTEGER  i = 0;
+		SQLINTEGER  native;
+		SQLWCHAR    state[7];
+		SQLWCHAR    text[256];
+		SQLSMALLINT len;
+		SQLRETURN   ret;
+		fprintf(stderr,
+			"\n"
+			"The driver reported the following diagnostics whilst running "
+			"%s\n\n",
+			fn);
+		do
+		{
+			ret = SQLGetDiagRec(type, handle, ++i, state, &native, text, sizeof(text), &len);
+			if (SQL_SUCCEEDED(ret))
+				printf("%s:%ld:%ld:%s\n", state, i, native, text);
+		} while (ret == SQL_SUCCESS);
+	}
+	// open connection to DataSource newSp
+	SQLRETURN dbConn(SQLHENV hEnv, SQLHDBC* hDBC) {
+		SQLWCHAR              szDSN[]    = L"newSp";       // Data Source Name buffer
+		SQLWCHAR              szUID[]    = L"root";		   // User ID buffer
+		SQLWCHAR              szPasswd[] = L"ragtinmor";   // Password buffer
+		SQLRETURN             fsts;
+
+		fsts = SQLAllocHandle(SQL_HANDLE_DBC, hEnv, hDBC);  // Allocate memory for the connection handle
+		if (!SQL_SUCCEEDED(fsts))	{
+			extract_error("SQLAllocHandle for dbc", hEnv, SQL_HANDLE_ENV);
+			exit(1);
+		}
+		// Connect to Data Source
+		fsts = SQLConnect(*hDBC, szDSN, SQL_NTS, szUID, SQL_NTS, szPasswd, SQL_NTS); // use SQL_NTS for length...NullTerminatedString
+		if (!SQL_SUCCEEDED(fsts))	{
+			extract_error("SQLAllocHandle for connect", hDBC, SQL_HANDLE_DBC);
+			exit(1);
+		}
+		return fsts;
+	}
+
+	MyDB(char **bindBuffer) : bindBuffer(bindBuffer){
+		SQLAllocEnv(&hEnv);
+		fsts = dbConn(hEnv, &hDBC);              // connect
+		if (fsts != SQL_SUCCESS && fsts != SQL_SUCCESS_WITH_INFO) { exit(1); }
+	};
+	~MyDB(){
+		SQLDisconnect(hDBC);           // Disconnect from datasource
+		SQLFreeConnect(hDBC); // Free the allocated connection handle
+		SQLFreeEnv(hEnv);    // Free the allocated ODBC environment handle
+	}
+	void prepare(SQLCHAR* thisSQL, int numCols) {
+		if (hStmt != NULL) {
+			SQLFreeStmt(hStmt, SQL_DROP);
+		}
+		fsts  =  SQLAllocStmt(hDBC, &hStmt); 	 // Allocate memory for statement handle
+		fsts  =  SQLPrepareA(hStmt, thisSQL, SQL_NTS);                 // Prepare the SQL statement	
+		fsts  =  SQLExecute(hStmt);                                     // Execute the SQL statement
+		if (!SQL_SUCCEEDED(fsts))	{ extract_error("SQLExecute get basic info ", hStmt, SQL_HANDLE_STMT);	exit(1); }
+		for (int i = 0; i < numCols; i++){
+			SQLBindCol(hStmt, i + 1, SQL_C_CHAR, bindBuffer[i], bufSize, &cbModel); // bind columns
+		}
+	}
+	void bind(int col, char *buffer) {
+		SQLBindCol(hStmt, col, SQL_C_CHAR, buffer, bufSize, &cbModel); // bind columns
+	}
+	SQLRETURN fetch(bool checkForErrors){
+		fsts = SQLFetch(hStmt);
+		if (checkForErrors){
+			if (fsts != SQL_SUCCESS && fsts != SQL_SUCCESS_WITH_INFO)	{ extract_error("SQLFetch", hStmt, SQL_HANDLE_STMT);	exit(1); }
+		}
+		return fsts;
+	}
+	SQLRETURN execute(bool checkForErrors){
+		fsts = SQLExecute(hStmt);
+		if (checkForErrors){
+			if (fsts != SQL_SUCCESS && fsts != SQL_SUCCESS_WITH_INFO)	{ extract_error("SQLExecute", hStmt, SQL_HANDLE_STMT);	exit(1); }
+		}
+		return fsts;
+	}
+
+};
+
---
+++ bberg/bberg/bberg.cpp
@@ -0,0 +1,104 @@
+// bberg.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Header.h"
+using namespace BloombergLP; 
+using namespace blpapi;
+using namespace std;
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	try{
+		// initialise
+		if (argc < 3){ cout << "Usage: startId stopId <optionalArguments>" << endl;  exit(0); }
+		int              startProductId  = argc > 1 ? _ttoi(argv[1]) : 34;
+		int              stopProductId   = argc > 2 ? _ttoi(argv[2]) : 1000;
+	
+
+		// init
+		const int        maxBufs(100);
+		const int        bufSize(1000);
+		SQLHENV          hEnv = NULL;		  // Env Handle from SQLAllocEnv()
+		SQLHDBC          hDBC = NULL;         // Connection handle
+		RETCODE          retcode;
+		char             lineBuffer[1000], charBuffer[1000];
+		char             **szAllBufs = new char*[maxBufs];
+		vector<int>      allProductIds; allProductIds.reserve(1000);
+		for (int i = 0; i < maxBufs; i++){
+			szAllBufs[i] = new char[bufSize];
+		}
+
+
+
+		// Bberg Setup Session parameters: port, buffer size, etc...
+		SessionOptions sessionOptions;
+		sessionOptions.setServerHost("127.0.0.1");
+		sessionOptions.setServerPort(8194);
+		Session session(sessionOptions); // Establish session
+		// Start Session
+		if (!session.start()) {
+			std::cerr << "Failed to start session." << std::endl;
+			return 1;
+		}
+		// Bberg open service
+		if (!session.openService("//blp/refdata")) {
+			std::cerr << "Failed to open //blp/refdata" << std::endl;
+			return 1;
+		}
+		Service ref = session.getService("//blp/refdata");
+
+
+
+
+		// open database
+		MyDB  mydb((char **)szAllBufs);
+
+		// get list of productIds
+		sprintf(lineBuffer, "%s%d%s%d%s", "select ProductId from product where ProductId>='", startProductId, "' and ProductId<='", stopProductId, "' and Matured='0'"); 	mydb.prepare((SQLCHAR *)lineBuffer, 1); 	retcode = mydb.fetch(true);
+		while (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+			int x(atoi(szAllBufs[0])); allProductIds.push_back(x);
+			retcode = mydb.fetch(false);
+		}
+
+		// get BID,ASK prices for productIds
+		sprintf(lineBuffer, "%s%d%s%d%s", "select ProductId, p.name, p.StrikeDate, cp.name, if (p.BbergTicker != '', p.BbergTicker, Isin) Isin, BbergPriceFeed from product p join institution cp on(p.CounterpartyId=cp.institutionid) where Isin != ''  and productid>='", startProductId, "' and ProductId<='", stopProductId, "' and Matured='0' order by ProductId; ");
+		mydb.prepare((SQLCHAR *)lineBuffer, 6); 	retcode = mydb.fetch(true);
+		while (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+			int id(atoi(szAllBufs[0]));
+			string tickerString = szAllBufs[4];
+			bool found = tickerString.find("Equity") == std::string::npos;
+			if (found) {
+				sprintf(charBuffer, "%s%s%s%s", "/isin/",szAllBufs[4], "@", szAllBufs[5]);
+			}
+			else {
+				strcpy(charBuffer, szAllBufs[4]);
+			}
+			double thisPrice = getBbergPrice(charBuffer, found ? "PX_BID" : "PX_LAST", ref, session);
+			retcode = mydb.fetch(false);
+		}
+		
+
+		// tidy up
+		session.stop();
+		cout << std::endl << "DONE" << std::endl;	
+	} // try
+
+	// tidy up
+	catch (out_of_range oor){
+		cerr << "Out of Range error \n" << oor.what();
+	}
+	catch (bad_alloc){
+		cerr << "Out of Memory error \n";
+	}
+	catch (length_error){
+		cerr << "Length error \n";
+	}
+	catch (...){
+		cerr << "unknown error \n";
+	}
+	return 0;
+}
+
