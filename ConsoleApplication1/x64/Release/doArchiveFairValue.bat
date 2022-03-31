39635b5db486cadac99abafa911a2886c5c10811
Art Noble <arnoble@btinternet.com>
2022-03-31 17:06:24 +0100

    doArchiveFairValue.bat doing full fair-value trace for some product at some date

--- ConsoleApplication1/x64/Release/doArchiveFairValue.bat
+++ ConsoleApplication1/x64/Release/doArchiveFairValue.bat
@@ -1,3 +1,25 @@
-rem  productId  dbServer asOfDate
+rem  productId  dbServer asOfDate   eg   10117 newSp  2022-03-11
 
-consoleApplication1.exe 10117 10117 40000 getMarketData dbServer:newSp  endDate:2022-03-11     arcCdsDate:2022-03-11 arcOnCurveDate:2022-03-11  arcCurveDate:2022-03-11 arcDivDate:2022-03-11 arcCorDate:2022-03-11 arcVolDate:2022-03-11  silent
+echo "use all archive data"
+consoleApplication1.exe %1 %1 40000 getMarketData dbServer:%2  endDate:%3     arcCdsDate:2022-03-11 arcOnCurveDate:2022-03-11  arcCurveDate:2022-03-11 arcDivDate:2022-03-11 arcCorDate:2022-03-11 arcVolDate:2022-03-11  silent
+
+echo "update vols"
+consoleApplication1.exe %1 %1 40000 getMarketData dbServer:%2  endDate:%3     arcCdsDate:2022-03-11 arcOnCurveDate:2022-03-11  arcCurveDate:2022-03-11 arcDivDate:2022-03-11 arcCorDate:2022-03-11 silent
+
+echo "update correlation"
+consoleApplication1.exe %1 %1 40000 getMarketData dbServer:%2  endDate:%3     arcCdsDate:2022-03-11 arcOnCurveDate:2022-03-11  arcCurveDate:2022-03-11 arcDivDate:2022-03-11  silent
+
+echo "update impDiv"
+consoleApplication1.exe %1 %1 40000 getMarketData dbServer:%2  endDate:%3     arcCdsDate:2022-03-11 arcOnCurveDate:2022-03-11  arcCurveDate:2022-03-11  silent
+
+echo "update onCurve"
+consoleApplication1.exe %1 %1 40000 getMarketData dbServer:%2  endDate:%3     arcCdsDate:2022-03-11 arcCurveDate:2022-03-11  silent
+
+echo "update curve"
+consoleApplication1.exe %1 %1 40000 getMarketData dbServer:%2  endDate:%3     arcCdsDate:2022-03-11  silent
+
+echo "update cds"
+consoleApplication1.exe %1 %1 40000 getMarketData dbServer:%2  endDate:%3     silent
+
+echo "update spots"
+consoleApplication1.exe %1 %1 40000 getMarketData dbServer:%2  silent
