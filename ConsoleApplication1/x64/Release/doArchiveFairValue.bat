06e7ae06cad6685c5a0a113de34f4357d7fe5700
Art Noble <arnoble@btinternet.com>
2022-03-31 16:56:41 +0100

    Add doArchiveFairValue.bat to fair-value some product at some date

---
+++ ConsoleApplication1/x64/Release/doArchiveFairValue.bat
@@ -0,0 +1,3 @@
+rem  productId  dbServer asOfDate
+
+consoleApplication1.exe 10117 10117 40000 getMarketData dbServer:newSp  endDate:2022-03-11     arcCdsDate:2022-03-11 arcOnCurveDate:2022-03-11  arcCurveDate:2022-03-11 arcDivDate:2022-03-11 arcCorDate:2022-03-11 arcVolDate:2022-03-11  silent
